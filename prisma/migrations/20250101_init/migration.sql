fdcf2adedeec87e59208db27d6cfc53e0c312191
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-11-16 21:50:42 +1300

    Delete prisma/migrations directory

--- prisma/migrations/20250101_init/migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "StickyEvent" (
-  "id" SERIAL PRIMARY KEY,
-  "shopDomain" TEXT NOT NULL,
-  "type" TEXT NOT NULL,
-  "productId" TEXT NOT NULL,
-  "variantId" TEXT NOT NULL,
-  "quantity" INTEGER NOT NULL,
-  "createdAt" TIMESTAMP DEFAULT NOW()
-);
