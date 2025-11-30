a96b8b3c36abea10afc9643f22533bd9eab44738
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-11-30 19:12:40 +1300

    Create Home.jsx

---
+++ web/frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { Card, Text } from "@shopify/polaris";
+import React from "react";
+
+export default function Home() {
+  return (
+    <Card>
+      <Card.Section>
+        <Text as="h2" variant="headingLg">
+          Sticky Add-to-Cart Bar Pro
+        </Text>
+        <Text as="p">
+          Welcome to your app dashboard. Use the sidebar to explore analytics &
+          settings.
+        </Text>
+      </Card.Section>
+    </Card>
+  );
+}
