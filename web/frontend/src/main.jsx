23ea7f15ccb7a1820e7e87d213228f653779890c
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-11-30 19:08:27 +1300

    Update main.jsx

--- web/frontend/src/main.jsx
+++ web/frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Providers from "./providers.jsx";
 import "@shopify/polaris/build/esm/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
