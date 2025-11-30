15db2cfa77d803492e1100f8a22921f417c484a6
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-11-30 19:41:08 +1300

    Update providers.jsx

--- web/frontend/src/providers.jsx
+++ web/frontend/src/providers.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
-import AppBridgeProvider from "@shopify/app-bridge-react";
 import { BrowserRouter } from "react-router-dom";
 
 export default function Providers({ children }) {
-  const searchParams = new URLSearchParams(window.location.search);
-  const host = searchParams.get("host");
-
-  const appBridgeConfig = {
-    apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
-    host,
-    forceRedirect: true,
-  };
-
   return (
-    <AppBridgeProvider config={appBridgeConfig}>
-      <PolarisProvider>
-        <BrowserRouter>{children}</BrowserRouter>
-      </PolarisProvider>
-    </AppBridgeProvider>
+    <PolarisProvider>
+      <BrowserRouter>{children}</BrowserRouter>
+    </PolarisProvider>
   );
 }
