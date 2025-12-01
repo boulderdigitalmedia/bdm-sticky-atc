22920379e30f42eb028b197f0445f796750ad588
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-01 22:53:34 +1300

    Update Home.jsx

--- web/frontend/src/pages/Home.jsx
+++ web/frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <page>
+    <page title="Dashboard">
     <Card>
       <Card.Section>
         <Text as="h2" variant="headingLg">
