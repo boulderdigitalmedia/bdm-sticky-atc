76f1bdce816a3f8f84e0812c429e5d62b0ee47e7
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-14 22:59:10 +1300

    Update providers.jsx

--- web/frontend/src/providers.jsx
+++ web/frontend/src/providers.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
-import { AppBridgeProvider } from "@shopify/app-bridge-react";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import { BrowserRouter } from "react-router-dom";
 
 export default function Providers({ children }) {
-  const params = new URLSearchParams(window.location.search);
-  const host = params.get("host");
-
-  if (!host) {
-    return <div>Missing Shopify host parameter</div>;
-  }
-
   return (
     <PolarisProvider i18n={enTranslations}>
-      <AppBridgeProvider
-        config={{
-          apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
-          host,
-          forceRedirect: true,
-        }}
-      >
-        <BrowserRouter>{children}</BrowserRouter>
-      </AppBridgeProvider>
+      <BrowserRouter>{children}</BrowserRouter>
     </PolarisProvider>
   );
 }
