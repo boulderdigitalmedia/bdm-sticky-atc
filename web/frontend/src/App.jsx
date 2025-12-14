7400f20ba13ecf507e97700892a81263fb9b02b8
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-14 22:54:16 +1300

    Update App.jsx

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// web/frontend/src/App.jsx
 import React from "react";
 import { Frame } from "@shopify/polaris";
 import AppRouter from "./router.jsx";
@@ -5,9 +6,7 @@ import Navigation from "./navigation.jsx";
 
 export default function App() {
   return (
-    <Frame
-      navigation={<Navigation />}
-    >
+    <Frame navigation={<Navigation />}>
       <AppRouter />
     </Frame>
   );
