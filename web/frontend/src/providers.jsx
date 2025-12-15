63374b2480945cd2bc3c3a0438191bbdef1adba6
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-15 23:02:58 +1300

    Update providers.jsx

--- web/frontend/src/providers.jsx
+++ web/frontend/src/providers.jsx
@@ -1,31 +1,12 @@
 import React from "react";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
-import { AppBridgeProvider } from "@shopify/app-bridge-react";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import { BrowserRouter } from "react-router-dom";
 
-/**
- * Shopify injects ?host= into the iframe URL
- */
-function getHost() {
-  const params = new URLSearchParams(window.location.search);
-  return params.get("host");
-}
-
 export default function Providers({ children }) {
-  const host = getHost();
-
-  const appBridgeConfig = {
-    apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
-    host,
-    forceRedirect: true,
-  };
-
   return (
     <PolarisProvider i18n={enTranslations}>
-      <AppBridgeProvider config={appBridgeConfig}>
-        <BrowserRouter>{children}</BrowserRouter>
-      </AppBridgeProvider>
+      <BrowserRouter>{children}</BrowserRouter>
     </PolarisProvider>
   );
 }
