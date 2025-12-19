95c501043a15e20e84d2db09858eabf125fad34c
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-20 10:47:41 +1300

    Update main.jsx

--- web/frontend/src/main.jsx
+++ web/frontend/src/main.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppProvider } from "@shopify/polaris";
+
+import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import enTranslations from "@shopify/polaris/locales/en.json";
+
+import { Provider as AppBridgeProvider } from "@shopify/app-bridge-react";
 
 import App from "./App.jsx";
 
+/**
+ * Shopify injects `host` into the iframe URL
+ * App Bridge REQUIRES this or it will hard crash
+ */
+const params = new URLSearchParams(window.location.search);
+const host = params.get("host");
+
 ReactDOM.createRoot(document.getElementById("app")).render(
-  <AppProvider i18n={enTranslations}>
-    <App />
-  </AppProvider>
+  <AppBridgeProvider
+    config={{
+      apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
+      host,
+      forceRedirect: true,
+    }}
+  >
+    <PolarisProvider i18n={enTranslations}>
+      <App />
+    </PolarisProvider>
+  </AppBridgeProvider>
 );
