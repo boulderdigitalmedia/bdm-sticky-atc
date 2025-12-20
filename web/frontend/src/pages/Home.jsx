2351a031e7564dea442335d4742dfe863cbf7f6d
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-21 09:58:08 +1300

    Update Home.jsx

--- web/frontend/src/pages/Home.jsx
+++ web/frontend/src/pages/Home.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import { Page, Card, Text } from "@shopify/polaris";
+import { Page, Card, Text, BlockStack } from "@shopify/polaris";
 
 export default function Home() {
   return (
-    <Page title="Sticky Add-to-Cart Bar Pro">
-      <Card>
-        <Text as="h2" variant="headingLg">
-          Dashboard
-        </Text>
-      </Card>
+    <Page title="Sticky Add to Cart Bar">
+      <BlockStack gap="400">
+        <Card>
+          <Text as="h2" variant="headingMd">
+            App Status
+          </Text>
+          <Text as="p">
+            Your Sticky Add to Cart Bar is installed and running.
+          </Text>
+        </Card>
+
+        <Card>
+          <Text as="h2" variant="headingMd">
+            What this app does
+          </Text>
+          <Text as="p">
+            Displays a persistent Add to Cart bar to increase conversion rate.
+          </Text>
+        </Card>
+      </BlockStack>
     </Page>
   );
 }
