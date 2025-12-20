f99ea2b070aa9b21e272165faa4cbc577e1fef96
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-20 20:53:36 +1300

    Update App.jsx

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -1,23 +1,11 @@
-import { Frame, Page, Layout, Card, Text } from "@shopify/polaris";
+import { Frame } from "@shopify/polaris";
+import AppRouter from "./router.jsx";
+import AppNavigation from "./navigation.jsx";
 
 export default function App() {
   return (
-    <Frame>
-      <Page title="Sticky Add To Cart Bar">
-        <Layout>
-          <Layout.Section>
-            <Card>
-              <Text variant="headingLg" as="h1">
-                Sticky Add-to-Cart Bar
-              </Text>
-
-              <Text as="p" tone="subdued">
-                App loaded successfully. Next step: analytics + dashboard.
-              </Text>
-            </Card>
-          </Layout.Section>
-        </Layout>
-      </Page>
+    <Frame navigation={<AppNavigation />}>
+      <AppRouter />
     </Frame>
   );
 }
