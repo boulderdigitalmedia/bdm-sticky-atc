5f486f5b40e59e94b2c13642372e3b8ef163165d
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-20 20:33:03 +1300

    Update main.jsx

--- web/frontend/src/main.jsx
+++ web/frontend/src/main.jsx
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { AppProvider as PolarisProvider } from "@shopify/polaris";
+import { AppProvider } from "@shopify/polaris";
 import enTranslations from "@shopify/polaris/locales/en.json";
-
-import { Provider } from "@shopify/app-bridge-react";
 
 import App from "./App.jsx";
 
-const params = new URLSearchParams(window.location.search);
-const host = params.get("host");
+console.log("React mounted successfully");
 
 ReactDOM.createRoot(document.getElementById("app")).render(
-  <Provider
-    config={{
-      apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
-      host,
-      forceRedirect: true,
-    }}
-  >
-    <PolarisProvider i18n={enTranslations}>
-      <App />
-    </PolarisProvider>
-  </Provider>
+  <AppProvider i18n={enTranslations}>
+    <App />
+  </AppProvider>
 );
