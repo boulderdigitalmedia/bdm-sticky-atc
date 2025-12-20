d8756b6079efb4111f38755e5264f60907345232
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-21 08:55:02 +1300

    Update main.jsx

--- web/frontend/src/main.jsx
+++ web/frontend/src/main.jsx
@@ -1,14 +1,25 @@
+import "@shopify/polaris/build/esm/styles.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppProvider } from "@shopify/polaris";
+import { AppBridgeProvider } from "@shopify/app-bridge-react";
 import enTranslations from "@shopify/polaris/locales/en.json";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("app")).render(
-  <AppProvider i18n={enTranslations}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AppProvider>
+const appBridgeConfig = {
+  apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
+  host: new URLSearchParams(window.location.search).get("host"),
+  forceRedirect: true,
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppBridgeProvider config={appBridgeConfig}>
+    <AppProvider i18n={enTranslations}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppProvider>
+  </AppBridgeProvider>
 );
