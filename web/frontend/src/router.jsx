39e348a0464e3c7eb24070eea6f0b2ce89d37f17
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-20 20:49:47 +1300

    Update router.jsx

--- web/frontend/src/router.jsx
+++ web/frontend/src/router.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 import Analytics from "./pages/Analytics.jsx";
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/analytics" element={<Analytics />} />
     </Routes>
   );
