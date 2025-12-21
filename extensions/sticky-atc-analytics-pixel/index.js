ee43d45b2386dcd34c661a77b72d456ecd972bf5
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-21 15:14:22 +1300

    Update index.js

--- extensions/sticky-atc-analytics-pixel/index.js
+++ extensions/sticky-atc-analytics-pixel/index.js
@@ -1,12 +1,12 @@
-import { register } from "@shopify/web-pixels-extension";
-
 register(({ analytics, browser }) => {
   analytics.subscribe("checkout_completed", (event) => {
     const checkout = event.data.checkout;
 
     fetch("https://sticky-add-to-cart-bar-pro.onrender.com/api/analytics/checkout", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({
         shop: browser?.shop?.domain,
         checkoutToken: checkout?.token,
