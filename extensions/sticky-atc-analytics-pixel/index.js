c28346be17a5e70cb1911d09bed3e8a3ab904066
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-21 15:39:15 +1300

    Update index.js

--- extensions/sticky-atc-analytics-pixel/index.js
+++ extensions/sticky-atc-analytics-pixel/index.js
@@ -1,17 +1,17 @@
-register(({ analytics, browser }) => {
-  analytics.subscribe("checkout_completed", (event) => {
-    const checkout = event.data.checkout;
+import { register } from "@shopify/web-pixels-extension";
 
-    fetch("https://sticky-add-to-cart-bar-pro.onrender.com/api/analytics/checkout", {
+register((api) => {
+  api.analytics.subscribe("checkout_completed", (event) => {
+    fetch("https://sticky-add-to-cart-bar-pro.onrender.com/apps/bdm-sticky-atc/track", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        shop: browser?.shop?.domain,
-        checkoutToken: checkout?.token,
-        totalPrice: checkout?.totalPrice?.amount,
-        currency: checkout?.currencyCode,
+        event: "checkout_completed",
+        shop: event.context.shop?.domain,
+        orderId: event.data?.checkout?.order?.id,
+        revenue: event.data?.checkout?.totalPrice?.amount,
+        currency: event.data?.checkout?.totalPrice?.currencyCode,
+        timestamp: Date.now(),
       }),
     });
   });
