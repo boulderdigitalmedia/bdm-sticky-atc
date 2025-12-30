7cd3869d261ea094649ac8bda11f3d00a7517e0b
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2025-12-30 09:22:53 +0000

    Add valid Shopify Web Pixel extension

--- extensions/sticky-atc-web-pixel/src/index.js
+++ extensions/sticky-atc-web-pixel/src/index.js
@@ -1,7 +1,5 @@
-// extensions/sticky-atc-web-pixel/src/index.js
-
 export default function register({ analytics }) {
   analytics.subscribe("page_viewed", (event) => {
-    console.log("Sticky ATC Pixel: page_viewed", event);
+    console.log("Sticky ATC Web Pixel", event);
   });
 }
