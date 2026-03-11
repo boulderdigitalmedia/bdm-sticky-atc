39d7d4d2d9a3d855ae21864d295449c50f59ac86
boulderdigitalmedia <jake@boulderdigitalmedia.com>
2026-03-11 10:19:45 +0000

    app.jsx

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
+import { Routes, Route } from "react-router-dom";
 
-export default function App({ children }) {
+import Dashboard from "./pages/Dashboard.jsx";
+
+export default function App() {
   return (
     <AppProvider i18n={{}}>
-      {children}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </AppProvider>
   );
 }
